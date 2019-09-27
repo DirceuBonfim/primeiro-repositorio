19dfd4f277ae8ef415e4e5b40ea1c25c4860f8b7
DBonfim <dbonfim@dbonfim.lan>
2019-09-26 23:17:13 -0300

    primeira importacao

---
+++ exerc2_33.java
@@ -0,0 +1,26 @@
+//exercicio 2.33 java como programar
+//Programa para calcular o IMC modificado
+
+import java.util.Scanner
+{
+	public class exerc2_33
+	{
+		int IMC;
+		int pesoEmQuilogramas;
+		int alturaEmMetros;
+
+		System.out.print("Digite o seu peso em quilogramas: ");
+		pesoEmQuilogramas = input.nextInt();
+
+		System.out.print("Digite a sua altura em metros: ");
+		alturaEmMetros = input.nextInt();
+
+		IMC = pesoEmQuilogramas/alturaEmMetros*alturaEmMetros;
+
+		if(IMC <= 18.5)
+			System.out.printf("Seu peso esta abaixo do normal", IMC);		if(IMC )	
+	}
+
+}
+
+
